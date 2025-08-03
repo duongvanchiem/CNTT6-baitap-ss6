4aa371cba158ecf1d77e51d60df092ba3cab6a8a
duongvanchiem <duongvanchien1702@gmail.com>
2025-08-03 21:01:59 +0700

    Add files via upload

--- it102_cntt6_hnks24_bai7ss6.c
+++ it102_cntt6_hnks24_bai7ss6.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 int main() {
-    int a, b;
+    int num1, num2;
     do {
         printf("Enter the first positive integer: ");
-        scanf("%d", &a);
+        scanf("%d", &num1);
         printf("Enter the second positive integer: ");
-        scanf("%d", &b);
-
-        if (a <= 0 || b <= 0) {
+        scanf("%d", &num2);
+        if (num1 <= 0 || num2 <= 0) {
             printf("Both numbers must be positive. Please try again.\n");
         }
-    } while (a <= 0 || b <= 0);
-    int x = a;
-    int y = b;
-    while (x != y) {
-        if (x > y)
-            x -= y;
-        else
-            y -= x;
-    }
-    printf("Greatest Common Divisor (GCD) of %d and %d is: %d\n", a, b, gcd);
+    } while (num1 <= 0 || num2 <= 0);
+    int result = gcd(num1, num2);
+    printf("The Greatest Common Divisor of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
